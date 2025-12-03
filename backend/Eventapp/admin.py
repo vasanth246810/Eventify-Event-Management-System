59ea2490f05ba1b0a1c79bd245646fc1bac56ecd
Vasanth S <vasanthkumar49480@gmail.com>
2025-12-03 20:49:23 +0530

    commit

--- backend/Eventapp/admin.py
+++ backend/Eventapp/admin.py
@@ -9,3 +9,19 @@ from .models import Events, UserProfile
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "is_admin", "created_on")
+
+
+@admin.register(Events)
+class EventsAdmin(admin.ModelAdmin):
+    list_display = (
+        "event_id",
+        "event_title",
+        "event_scheduled_date",
+        "event_location",
+        "event_available_seats",
+        "event_price",
+        "is_sold_out"
+    )
+    list_filter = ("is_sold_out", "event_scheduled_date")
+    search_fields = ("event_title", "event_location", "location_name")
+    readonly_fields = ("event_id",)
